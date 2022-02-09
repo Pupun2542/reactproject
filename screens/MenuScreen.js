b3202b40b3391c99e463384aba81b3fbdda290a2
Pupun2542 <ppupun2542@gmail.com>
2022-02-09 10:34:07 +0700

    push 2

--- screens/MenuScreen.js
+++ screens/MenuScreen.js
@@ -1,21 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View } from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Button,
+  ListItem,
+  Text,
+  Icon,
+  Left,
+  Body,
+  Right,
+  Switch,
+} from "native-base";
+import React from "react";
 
-const MenuScreen = () => {
+const MenuScreen = ( {navigation} ) => {
   return (
     <View>
       <Text style={styles.style}>MenuScreen</Text>
+      <Content>
+        <ListItem icon
+            style={{marginBottom:10, marginTop:10}}
+            onPress={()=>{navigation.navigate("HomeStack")}}
+        >
+          <Left>
+            <Button style={{ backgroundColor: "#FF9501" }}>
+              <Icon active name="home" />
+            </Button>
+          </Left>
+          <Body>
+            <Text>หน้าหลัก</Text>
+          </Body>
+          <Right>
+            <Icon active name="arrow-forward" />
+          </Right>
+        </ListItem>
+        <ListItem icon>
+          <Left>
+            <Button style={{ backgroundColor: "#007AFF" }}>
+              <Icon active name="wifi" />
+            </Button>
+          </Left>
+          <Body>
+            <Text>สินค้า</Text>
+          </Body>
+          <Right>
+            <Icon active name="arrow-forward" />
+          </Right>
+        </ListItem>
+      </Content>
     </View>
-  )
-}
+  );
+};
 
-export default MenuScreen
+export default MenuScreen;
 
 const styles = StyleSheet.create({
-    style:{
-        color:'blue',
-        fontSize:20,
-        fontWeight:'bold',
-        padding:20
-    }
-})
+  style: {
+    color: "blue",
+    fontSize: 20,
+    fontWeight: "bold",
+    padding: 20,
+  },
+});
